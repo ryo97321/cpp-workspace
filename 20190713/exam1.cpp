30dedddff0b9c54c628f4582a7505f3637a7778b
mHALr <ryo97321@gmail.com>
2019-07-13 14:54:27 +0900

    sixth

--- 20190713/exam1.cpp
+++ 20190713/exam1.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+
+    for(int i=0; i<N; i++) {
+        cin >> A.at(i);
+    }
 
     int count = 0;
-    for(char c : s) {
-        if(c == '1') count++;
+
+    while(true) {
+        bool allEven = true;
+
+        for(int i=0; i<N; i++) {
+            if(A.at(i) % 2 != 0) {
+                allEven = false;
+                break;
+            }
+        }
+
+        if(allEven) {
+            for(int j=0; j<N; j++) {
+                A.at(j) /= 2;
+            }
+            count++;
+        } else {
+            break;
+        }
     }
 
     cout << count << endl;
