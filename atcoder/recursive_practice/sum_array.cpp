740351e68da64a3527844cda40f885575bcd225e
mHALr <ryo97321@gmail.com>
2020-01-31 08:39:44 +0900

    add sum_array.cpp

--- atcoder/recursive_practice/sum_array.cpp
+++ atcoder/recursive_practice/sum_array.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int array_sum_from_i(vector<int> &data, int i) {
+    // ベースケース
+    if (i == data.size()) {
+        return 0;
+    }
+
+    // 再起ステップ
+    int s = array_sum_from_i(data, i+1);
+    return data.at(i) + s;
+}
+
+int array_sum(vector<int> &data) {
+    return array_sum_from_i(data, 0);
+}
+
 int main() {
-    cout << "hello" << endl;
+    vector<int> a = {0, 3, 9, 1, 5};
+    cout << array_sum(a) << endl;
 }
