0d9d4a83e7501462c3cbe495833724f0c998e92b
mHALr <ryo97321@gmail.com>
2020-02-01 20:08:39 +0900

    add ex26.cpp (not completed)

---
+++ atcoder/cpp_tutorial/chap3/ex26.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i=0; i<N; i++) {
+        string func_name;
+        cin >> func_name;
+    }
+}
