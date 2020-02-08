b2f138e6de460250daa3f8abd5f07a1e05fcbc9b
mHALr <ryo97321@gmail.com>
2020-02-08 22:05:12 +0900

    finished

--- atcoder/past_questions/abc065B.cpp
+++ atcoder/past_questions/abc065B.cpp
@@ -1,66 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// ボタン2だけが光っているかどうか
-bool is_only_button2(vector<bool> &light) {
-    bool result = true;
-
-    for(int i=1; i<light.size(); i++) {
-        if (i == 2) {
-            continue;
-        } else {
-            if (light[i] == true) {
-                result = false;
-                break;
-            }
-        }
-    }
-
-    return result;
-}
-
 int main() {
     int N;
     cin >> N;
 
-    // ボタン1のライトだけ光っているとき light[1] = true
-    vector<bool> light((N+1), false);
+    vector<int> vec((N+1), 0);
 
-    // ボタン1を押せば３が光るとき button[1] = 3
-    vector<int> button((N+1), 0);
-
-    // そのボタンをすでに押したかどうか
-    // すでに押されたのに、また呼ばれればループしていると気づくことができる
-    // 例. ボタン1がすでに押されている -> push_checklist = true
-    vector<bool> push_checklist((N+1), false);
-
-    int light_number;
     for(int i=1; i<=N; i++) {
-        cin >> light_number;
-        button[i] = light_number;
+        cin >> vec[i];
     }
 
-    int count = 0;
-    int push_button_numner = 1;
-
-    while(count != -1) {
-
-        // すでに押されているボタンがまた呼ばれればループしている
-        if (push_checklist[push_button_numner] == true) {
-            count = -1;
-        } else {
-            light[button[push_button_numner]] = true;
-            light[push_button_numner] = false;
-            push_checklist[push_button_numner] = true;
-            push_button_numner = button[push_button_numner];
-            count++;
-        }
-
-        if (is_only_button2(light)) {
-            break;
+    int push_number = 1;
+    for(int i=1; i<=N; i++) {
+        push_number = vec[push_number];
+        if(push_number == 2) {
+            cout << i << endl;
+            return 0;
         }
     }
 
-    cout << count << endl;
-    return 0;
+    cout << -1 << endl;
 }
