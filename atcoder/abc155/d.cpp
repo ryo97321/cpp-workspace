097b6105fb60ef3ae165c8ed25b24fe49e2e4c39
mHALr <ryo97321@gmail.com>
2020-02-17 08:08:43 +0900

    add abc155/

---
+++ atcoder/abc155/d.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    vector<ll> vec(N);
+    for(int i=0; i<N; i++) cin >> vec[i];
+
+    return 0;
+}
